55ba5c7c04094432670ecb46924c76fa04676d85
franpassa <francopassarelli94@gmail.com>
2020-09-11 14:02:38 -0300

    Creo vinculador en diagrama de clases, falta la parte de las condiciones

---
+++ tpAnual/Context.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using MySql.Data.EntityFramework;
+using TPANUAL;
+
+namespace Bibliotecas
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    class Context : DbContext
+    {
+        public DbSet<Organizacion> Organizaciones { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<EntidadBase> EntidadesBase { get; set; }
+
+        public DbSet<EntidadJuridica> EntidadesJuridicas { get; set; }
+
+        public Context() : base("ContextDB")
+        {
+            Database.SetInitializer<Context>(new CreateDatabaseIfNotExists<Context>());
+        }
+    }
+}
