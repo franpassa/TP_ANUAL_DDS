fa9ef1fed901e12ceb136a55e684c4b9c64e36c8
Nacho-Keiniger <37520232+Nacho-Keiniger@users.noreply.github.com>
2020-11-04 17:03:23 -0300

    subo template de interfaz

---
+++ tpAnual/INTERFAZ/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TPANUAL;
+
+namespace INTERFAZ.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Ingresos()
+        {
+            return View();
+        }
+
+        public ActionResult Organizaciones()
+        {
+            return View();
+        }
+
+        public ActionResult Presupuestos()
+        {
+            return View();
+        }
+
+        public ActionResult Proveedores()
+        {
+            return View();
+        }
+    }
+}
